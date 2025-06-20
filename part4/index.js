f50f6f94839c1fe1a73650f44c2cc8cba076d5ee
Santeri Hildén <santeri.hilden@aalto.fi>
2025-06-20 15:43:40 +0300

    Part 4 started, 4.1 done

---
+++ part4/index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cros from 'cors'
+import dotenv from 'dotenv'
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+dotenv.config()
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
