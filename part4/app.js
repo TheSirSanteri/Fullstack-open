7ddb38ede17887bd22ad0d532155217643a90b74
Santeri <Santeri.hilden@gmail.com>
2025-07-17 19:01:19 +0300

    4.18 Done

--- part4/app.js
+++ part4/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs.js')
 const usersRouter = require('./controllers/users.js')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -10,6 +11,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use((error, req, res, next) => {
   if (error.name === 'ValidationError') {
@@ -18,6 +20,12 @@ app.use((error, req, res, next) => {
   if (error.name === 'MongoServerError' && error.code === 11000) {
     return res.status(400).json({ error: 'Username must be unique' })
   }
+  if (error.name === 'JsonWebTokenError') {
+  return res.status(401).json({ error: 'invalid token' })
+}
+if (error.name === 'TokenExpiredError') {
+  return res.status(401).json({ error: 'token expired' })
+}
   next(error)
 })
 
--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog.js')
 const User = require('../models/user.js')
 
+
 const blogsRouter = express.Router()
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -12,34 +22,37 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response, next) => {
   const { title, url, author, likes } = request.body
 
-  if (!title || !url) {
-    return response.status(400).end()
-  }
-
+  const token = getTokenFrom(request)
+  let decodedToken
   try {
-    const user = await User.findOne()
-    if (!user) {
-      return response.status(400).json({ error: 'no user available' })
-    }
-
-    const blog = new Blog({
-      title,
-      author,
-      url,
-      likes: likes || 0,
-      user: user._id
-    })
-
-    const savedBlog = await blog.save()
-
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-
-    const populated = await savedBlog.populate('user', { username: 1, name: 1 })
-    return response.status(201).json(populated.toJSON())
-  } catch (error) {
-    next(error)
+    decodedToken = jwt.verify(token, process.env.SECRET)
+  } catch (err) {
+    return response.status(401).json({ error: 'token missing or invalid' })
   }
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  if (!user) {
+    return response.status(400).json({ error: 'user not found' })
+  }
+
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes: likes || 0,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  const populated = await savedBlog.populate('user', { username: 1, name: 1 })
+  return response.status(201).json(populated.toJSON())
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
